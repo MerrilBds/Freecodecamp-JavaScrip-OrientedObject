bb1655f495614febce4abdfe9273e262782e650f
MerrilBds <Bdsmerril@gmail.com>
2023-07-09 19:11:32 +0200

    leçon 25 fin

--- #25.ImmediatelyInvokedFunctionExpression(IIFE)/invoke.js
+++ #25.ImmediatelyInvokedFunctionExpression(IIFE)/invoke.js
@@ -1,4 +1,9 @@
 //A common pattern in JavaScript is to execute a function as soon as it is declared:
-(function makeNest() {
+(function () {
     console.log("A cozy nest is ready");
 })();
+/*Note that the function has no name and is not stored in a variable. 
+ * The two parentheses () at the end of the function expression 
+ * cause it to be immediately executed or invoked. 
+ * This pattern is known as an immediately invoked function expression or IIFE.
+*/
