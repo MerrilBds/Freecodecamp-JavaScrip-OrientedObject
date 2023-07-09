aff97cc5665b7de7c23fd1b6eab5b0868c31f9a3
MerrilBds <Bdsmerril@gmail.com>
2023-07-09 16:59:38 +0200

    leçon 5 fin

---
+++ #05.DefineConstructorFunction/cons.js
@@ -0,0 +1,10 @@
+/*
+Constructors are functions that create new objects. They define properties and
+ behaviors that will belong to the new object. 
+Think of them as a blueprint for the creation of new objects.
+*/
+function Dog() {
+    this.name = "Alfie";
+    this.color = "White";
+    this.numLegs = 4;
+}
