d07496c74178d4d9f2f4f4e609de53959110afee
MerrilBds <Bdsmerril@gmail.com>
2023-07-09 17:02:40 +0200

    leçon 6 fin

---
+++ #06.UseConstructorToCreateNewObj/newObj.js
@@ -0,0 +1,17 @@
+/* 
+example : function Bird() {
+  this.name = "Albert";
+  this.color  = "blue";
+  this.numLegs = 2;
+}
+
+let blueBird = new Bird();
+*/
+
+function Dog() {
+    this.name = "Rupert";
+    this.color = "brown";
+    this.numLegs = 4;
+}
+
+let hound = new Dog();
